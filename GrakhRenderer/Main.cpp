fd41ab8a8a74145ffcb709770c483e70b9e77aff
Roman <nash.roma37@gmail.com>
2023-02-23 15:52:48 +0500

    Filled triangles with barycentric coordinates

--- GrakhRenderer/Main.cpp
+++ GrakhRenderer/Main.cpp
@@ -55,12 +55,12 @@ void Triangle(IntVector2 point0, IntVector2 point1, IntVector2 point2, TGAImage*
 	Line(point2.x, point2.y, point0.x, point0.y, image, color);
 }
 
-void FilledTriangle(IntVector2 point0, IntVector2 point1, IntVector2 point2, TGAImage* image, TGAColor color) {
+void FilledTriangleLineSweeping(IntVector2 point0, IntVector2 point1, IntVector2 point2, TGAImage* image, TGAColor color) {
 	if (point0.y > point1.y) std::swap(point0, point1);
 	if (point0.y > point2.y) std::swap(point0, point2);
 	if (point1.y > point2.y) std::swap(point1, point2);
 
-	const int totalHeight = point2.y - point0.y+1;
+	const int totalHeight = point2.y - point0.y + 1;
 	const int firstSegmentHeight = point1.y - point0.y + 1;
 	float boundaryPartThigh;
 	float boundaryPartBase;
@@ -94,6 +94,51 @@ void FilledTriangle(IntVector2 point0, IntVector2 point1, IntVector2 point2, TGA
 	}
 }
 
+FloatVector3 ToBarycentricCoordinates(IntVector2 point, IntVector2* points) {
+	FloatVector3 crossProduct =
+		FloatVector3(points[2].x - points[0].x, points[1].x - points[0].x, points[0].x - point.x) ^
+		FloatVector3(points[2].y - points[0].y, points[1].y - points[0].y, points[0].y - point.y);
+
+	if (std::abs(crossProduct.z) < 1)
+		return FloatVector3(-1, 1, 1);
+
+	return FloatVector3(
+		1. - (crossProduct.x + crossProduct.y) / crossProduct.z,
+		crossProduct.x / crossProduct.z,
+		crossProduct.y / crossProduct.z
+	);
+}
+
+inline bool IsBarycentricValid(FloatVector3 barycentric) {
+	return barycentric.x >= 0 && barycentric.y >= 0 && barycentric.z >= 0;
+}
+
+void FilledTriangleBarycentric(IntVector2* points, TGAImage* image, TGAColor color) {
+	int imageWidth = image->GetWidth(),
+		imageHeight = image->GetHeight();
+
+	IntVector2 bboxMin(imageWidth - 1, imageHeight - 1);
+	IntVector2 bboxMax(0, 0);
+
+	for (int i = 0; i < 3; i++) {
+		bboxMin.x = std::min(points[i].x, bboxMin.x);
+		bboxMax.x = std::max(points[i].x, bboxMax.x);
+
+		bboxMin.y = std::min(points[i].y, bboxMin.y);
+		bboxMax.y = std::max(points[i].y, bboxMax.y);
+	}
+
+	IntVector2 iterationPoint;
+	FloatVector3 barycentricScreenCoords;
+	for (iterationPoint.x = bboxMin.x; iterationPoint.x < bboxMax.x; iterationPoint.x++) {
+		for (iterationPoint.y = bboxMin.y; iterationPoint.y < bboxMax.y; iterationPoint.y++) {
+			barycentricScreenCoords = ToBarycentricCoordinates(iterationPoint, points);
+			if (IsBarycentricValid(barycentricScreenCoords))
+				image->Set(iterationPoint.x, iterationPoint.y, color);
+		}
+	}
+}
+
 IntVector2 ToScreenCoordinates(FloatVector3 vector, int width, int height) {
 	return IntVector2((vector.x + 1.) * width / 2, (vector.y + 1.) * height / 2);
 }
@@ -104,19 +149,17 @@ int main(int argc, char** argv) {
 
 	TGAImage image(width, height, TGAImage::RGB);
 
-	FilledTriangle(IntVector2(1000, 687), IntVector2(50, 687), IntVector2(800, 687), &image, Green);
-
 	Model model("obj/african_head.obj");
 	for (int i = 0; i < model.FaceAmount(); i++) {
-
 		std::vector<int> face = model.Face(i);
 
 		IntVector2 vectors[3];
 		for (int i = 0; i < 3; i++) {
 			vectors[i] = ToScreenCoordinates(model.Vertice(face[i]), width, height);
 		}
-
-		FilledTriangle(vectors[0], vectors[1], vectors[2], &image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
+		std::cerr << "face " << i << std::endl;
+		FilledTriangleBarycentric(vectors, &image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
+		//FilledTriangleLineSweeping(vectors[0], vectors[1], vectors[2], &image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
 	}
 
 	image.FlipVertically();
